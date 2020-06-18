5bd8a599831e4aa2c1873c90de2f26d72917ca34
gui <kiicho@kiicho.cc>
2020-06-18 18:42:55 +0800

    1

--- bahaBye-kan.js
+++ bahaBye-kan.js
@@ -1,4 +1,16 @@
-import(simpList.js);
+// ==UserScript==
+// @name         bahaBye-kan
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://ani.gamer.com.tw/animeVideo.php*
+// @grant        none
+// @require      https://greasyfork.org/scripts/405629-simplist/code/simpList.js?version=817690
+// ==/UserScript==
+
+
+
 var subs=document.getElementsByClassName('sub_content');
 for(var i=0;i<subs<subs.length;i++){
     var innersub=subs[i].innerHTML;
