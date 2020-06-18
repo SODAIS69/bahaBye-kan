1c3ef63484d9a38bb6c09999db6b52bc2f6c6464
gui <kiicho@kiicho.cc>
2020-06-19 00:11:45 +0800

    穩定版本

--- bahaBye-kan.js
+++ bahaBye-kan.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         bahaBye-kan - 巴哈掰簡
+// @name         bahaBye-kan - 巴哈8簡
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  💡過濾巴哈姆特動畫瘋簡體字彈幕用腳本⚾
 // @author       sodais69
 // @match        https://ani.gamer.com.tw/animeVideo.php*
@@ -9,6 +9,25 @@
 // @require      https://greasyfork.org/scripts/405629-simplist/code/simpList.js?version=817690
 // ==/UserScript==
 
+//個人化設定
+
+    //簡體字留言的處理方式
+    /*
+    如果修改 filterStyle 後面的數字成
+        1   文字會改成「根據相關法律該留言已經被隱藏了。」
+        2   文字會被消失
+    */
+    var filterStyle=1;
+
+
+
+
+    var  filterStr=' ';
+    if(filterStyle==1){
+        filterStr='「⚠️根據相關法律該留言已經被隱藏了。⚠️」';
+    }else{
+        filterStr=' ';
+    }
 window.addEventListener('load', function() {
     GM_log('hello world');
 
@@ -28,8 +47,8 @@ for(var i=0;i<danmu.length;i++){
     Object.keys(TongWen_st).every(key=>{
        var searchResult=innersub.indexOf(key);
       if(searchResult!=-1){
-          GM_log('Simplified Chinese found! content:'+innersub+" Bingo one:"+innersub.substr(searchResult,1));
-
+          GM_log('Simplified Chinese found! content:'+innersub+" Bingo word:"+innersub.substr(searchResult,1));
+          danmu[i]['text']=filterStr;
         return false;
       }else{
         return true;
