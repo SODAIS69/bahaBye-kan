473dd993281eb3b52aa55ea1c4f1e1c874782048
gui <kiicho@kiicho.cc>
2020-06-18 21:50:17 +0800

    程式基本可辨識簡字，新增描述性資訊

--- bahaBye-kan.js
+++ bahaBye-kan.js
@@ -1,23 +1,39 @@
 // ==UserScript==
-// @name         bahaBye-kan
+// @name         bahaBye-kan - 巴哈掰簡
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  💡過濾巴哈姆特動畫瘋簡體字彈幕用腳本⚾
+// @author       sodais69
 // @match        https://ani.gamer.com.tw/animeVideo.php*
-// @grant        none
+// @grant        GM_log
 // @require      https://greasyfork.org/scripts/405629-simplist/code/simpList.js?version=817690
 // ==/UserScript==
 
+window.addEventListener('load', function() {
+    GM_log('hello world');
+
+   var self  = animefun;
+  while(self.danmuLoaded!=true){
+  GM_log('waiting');
+    }
+  var danmu=self.danmu;
 
 
-var subs=document.getElementsByClassName('sub_content');
-for(var i=0;i<subs<subs.length;i++){
-    var innersub=subs[i].innerHTML;
-    
-    TongWen_st.forEach(function(key){
-        if(innersub.indexOf(key)!=-1){
-            innersub.empty();
-        }
-    });
+  GM_log('subs length:'+danmu.length);
+for(var i=0;i<danmu.length;i++){
+  var innersub=danmu[i]['text'];
+
+    //GM_log('i='+i+':'+innersub);
+
+    Object.keys(TongWen_st).every(key=>{
+       var searchResult=innersub.indexOf(key);
+      if(searchResult!=-1){
+          GM_log('Simplified Chinese found! content:'+innersub+" Bingo one:"+innersub.substr(searchResult,1));
+
+        return false;
+      }else{
+        return true;
+      }
+  });
 }
+}, false);
